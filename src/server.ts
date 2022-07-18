bcb60020a185b0dac7449a81f6997c485a751163
Abhishek-thakur1 <abhishekcool1804@gmail.com>
2022-07-18 19:03:03 +0530

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import cookieParser from "cookie-parser";
+import express from "express";
+// import { BAD_REQUEST } from "http-status-codes";
+// import BaseRouter from "./routes";
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+// Add APIs
+// app.use("/api", BaseRouter);
+// Export express instance
+export default app;
