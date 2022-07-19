1d68e41e6aea612846c29d8b4e75897f3a1273cf
Abhishek-thakur1 <abhishekcool1804@gmail.com>
2022-07-19 20:25:03 +0530

    password hashing before storing it to DB

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, Index, Column } from "typeorm";
+import { Entity, Index, Column, BeforeInsert } from "typeorm";
+import bcrypt from "bcryptjs";
 import Model from './model.entity'
 
 export enum RoleEnumType  {
@@ -40,6 +41,20 @@ export class User extends Model {
         default: false,
     })
     verified: boolean;
+
+    // 👇 Hash password before saving to database
+    @BeforeInsert()
+    async hashPassword() {
+        this.password = await bcrypt.hash(this.password, 12);
+    }
+
+    // 👇 Validate password
+    static async comparePasswords(
+        candidatePassword: string,
+        hashedPassword: string
+    ) {
+        return await bcrypt.compare(candidatePassword, hashedPassword);
+    }
 
     toJSON() {
         return { ...this, password: undefined, verified: undefined };
