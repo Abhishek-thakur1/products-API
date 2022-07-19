1e75e1a63ba5e7e6eda2c94e81322dd88950a285
Abhishek-thakur1 <abhishekcool1804@gmail.com>
2022-07-19 18:23:21 +0530

    rearranged and restructured...

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import "./LoadEnv"; 
+require("dotenv").config();
 import app from "./Server";
 import logger from "./shared/Logger";
-import PostgresDataSource from "./db"
-
-
+import validateEnv from './utils/validEnv'
+import PostgresDataSource from "./db";
 
 // Start the server
 const port = Number(process.env.PORT || 3000);
 app.listen(port, () => {
+    validateEnv();
     logger.info(`Server🚀 is running at http://localhost:${port}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import cookieParser from "cookie-parser";
 import express, { Request, Response, NextFunction } from "express";
+import morgan from 'morgan'
 import logger from "./shared/Logger";
 import { ReasonPhrases, StatusCodes, getReasonPhrase, getStatusCode} from "http-status-codes";
 // import BaseRouter from "./routes";
@@ -10,6 +11,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// Show routes called in console during development
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
 // Add APIs
 // app.use("/api", BaseRouter);
