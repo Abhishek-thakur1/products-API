033d27adb3255d2ffcd6ae4ffc323a132d155f34
Abhishek-thakur1 <abhishekcool1804@gmail.com>
2022-07-21 20:35:33 +0530

    added JWT auth verify E-mail

---
+++ src/utils/jwt.ts
@@ -0,0 +1,37 @@
+import config from "config";
+import jwt, { SignOptions } from "jsonwebtoken";
+
+// Sign Access or Refresh Token
+export const signJwt = (
+    payload: object,
+    keyName: 'accessTokenPrivateKey' | 'refreshTokenPrivateKey',
+    options: SignOptions
+) => {
+    const privateKey = Buffer.from(
+        config.get<string>(keyName),
+        'base64'
+    ).toString('ascii');
+    return jwt.sign(payload, privateKey, {
+        ...(options && options),
+        algorithm: 'RS256'
+    })
+}
+
+
+
+export const verifyJwt = <T>(
+    token: string,
+    keyName: 'accessTokenPublicKey' | 'refreshTokenPublicKey'
+): T | null => {
+    try {
+        const publicKey = Buffer.from(
+            config.get<string>(keyName),
+            'base64'
+        ).toString('ascii');
+        const decoded = jwt.verify(token, publicKey) as T;
+
+        return decoded;
+    } catch (error) {
+        return null;
+    }
+};
