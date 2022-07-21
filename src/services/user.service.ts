f0c68a014ebc76983fa3447e1ca7332188550fe1
Abhishek-thakur1 <abhishekcool1804@gmail.com>
2022-07-21 20:07:35 +0530

    created Services to Interact with the Database

---
+++ src/services/user.service.ts
@@ -0,0 +1,28 @@
+import {User} from '../entities/user.entity'
+import { createUserInput } from "../schemas/user.schema";
+import {AppDataSource} from '../utils/data-source'
+
+const userRepository = AppDataSource.getRepository(User);
+
+
+export const createUser = async (input: createUserInput) => {
+
+    return (await AppDataSource.manager.save(
+        AppDataSource.manager.create(User, input)
+    )) as User;
+}
+
+
+export const findUserByEmail = async ({ email }: { email: string }) => {
+    return await userRepository.findOneBy({email})
+}
+
+
+export const findUserById = async (userId: string) => {
+    return await userRepository.findOneBy({ id: userId });
+};
+
+export const findUser = async (query: Object) => {
+    return await userRepository.findOneBy(query);
+};
+
