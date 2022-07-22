6271dc2a7bf68e5c972105e3f64cfdd16e4f32be
Abhishek-thakur1 <abhishekcool1804@gmail.com>
2022-07-22 17:53:03 +0530

    added token services

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,3 +1,6 @@
+import config from 'config';
+import redisClient from 'src/utils/connectRedis';
+import { signJwt } from 'src/utils/jwt';
 import { DeepPartial } from 'typeorm';
 import {User} from '../entities/user.entity'
 import { createUserInput } from "../schemas/user.schema";
@@ -29,5 +32,24 @@ export const findUserById = async (userId: string) => {
 
 export const findUser = async (query: Object) => {
     return await userRepository.findOneBy(query);
+};
+
+
+export const signTokens = async (user: User) => {
+    // 1. Create Session
+    redisClient.set(user.id, JSON.stringify(user), {
+        EX: config.get<number>("redisCacheExpiresIn") * 60,
+    });
+
+    // 2. Create Access and Refresh tokens
+    const access_token = signJwt({ sub: user.id }, "accessTokenPrivateKey", {
+        expiresIn: `${config.get<number>("accessTokenExpiresIn")}m`,
+    });
+
+    const refresh_token = signJwt({ sub: user.id }, "refreshTokenPrivateKey", {
+        expiresIn: `${config.get<number>("refreshTokenExpiresIn")}m`,
+    });
+
+    return { access_token, refresh_token };
 };
 
