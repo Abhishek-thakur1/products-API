87e8eeada708d3a9fd9b11bdac876d470db94fa4
Abhishek-thakur1 <abhishekcool1804@gmail.com>
2022-07-24 18:03:37 +0530

    Register User Controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,7 @@
-import {CookieOptions, NextFunction, Response, Request} from 'express'
+import { CookieOptions, NextFunction, Response, Request } from 'express'
+import {createUserInput} from '../schemas/user.schema'
 import config = require('config')
+import { createUser } from 'src/services/user.service'
 
 
 const cookieOptions: CookieOptions = {
@@ -23,4 +25,39 @@ const refreshTokenCookieOptions: CookieOptions = {
         Date.now() + config.get<number>("refreshTokenExpiresIn") * 60 * 1000 
     ),
     maxAge: config.get<number>("refreshTokenExpiresIn") *60 * 1000
+}
+
+
+// Handling user registration
+export const registerUserHandler = async(
+    req: Request<{}, {}, createUserInput>,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const { firstName, lastName, password, email } = req.body
+        
+        const user = await createUser({
+            firstName,
+            lastName,
+            email: email.toLowerCase(),
+            password
+        });
+
+        res.status(201).json({ 
+            status: "success",
+            data: {
+                user
+            }
+        });
+
+    } catch ( err : any) {
+        if (err.code === '23505') {
+            return res.status(409).json({ 
+                status: 'fail',
+                message: ''
+            })
+        }
+        next(err);
+    }
 }
