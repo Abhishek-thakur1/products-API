15fe3b377db271710413f41de16b7b2e0b97a208
Abhishek-thakur1 <abhishekcool1804@gmail.com>
2022-07-24 18:49:39 +0530

    updated index.ts for handling routes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 require("dotenv").config();
-import express, { Response } from "express";
-import cookieParser from "cookie-parser";
-import morgan from "morgan";
+import express, { NextFunction, Request, Response } from "express";
 import config from "config";
-import validateEnv from "./utils/validEnv";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import { AppDataSource } from "./utils/data-source";
-import logger from './shared/Logger'
+import AppError from "./utils/appError";
+import authRouter from "./routes/auth.routes";
+import userRouter from "./routes/user.routes";
+import validateEnv from "./utils/validEnv";
+import logger from "./shared/Logger";
 import redisClient from "./utils/connectRedis";
 
 AppDataSource.initialize()
@@ -19,23 +23,28 @@ AppDataSource.initialize()
         const app = express();
 
         // MIDDLEWARE
-        app.use(express.json());
         app.use(express.urlencoded({ extended: true }));
-        app.use(cookieParser());
-
-        if (process.env.NODE_ENV === "development") {
-            app.use(morgan("dev"));
-        }
 
         // 1. Body parser
+        app.use(express.json({ limit: "10kb" }));
 
         // 2. Logger
+        if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
         // 3. Cookie Parser
+        app.use(cookieParser());
 
         // 4. Cors
+        app.use(
+            cors({
+                origin: config.get<string>("origin"),
+                credentials: true,
+            })
+        );
 
         // ROUTES
+        app.use("/api/auth", authRouter);
+        app.use("/api/users", userRouter);
 
         // HEALTH CHECKER
         app.get("/api/healthchecker", async (_, res: Response) => {
@@ -47,8 +56,27 @@ AppDataSource.initialize()
         });
 
         // UNHANDLED ROUTE
+        app.all("*", (req: Request, res: Response, next: NextFunction) => {
+            next(new AppError(404, `Route ${req.originalUrl} not found`));
+        });
 
         // GLOBAL ERROR HANDLER
+        app.use(
+            (
+                error: AppError,
+                req: Request,
+                res: Response,
+                next: NextFunction
+            ) => {
+                error.status = error.status || "error";
+                error.statusCode = error.statusCode || 500;
+
+                res.status(error.statusCode).json({
+                    status: error.status,
+                    message: error.message,
+                });
+            }
+        );
 
         const port = config.get<number>("port");
         app.listen(port);
