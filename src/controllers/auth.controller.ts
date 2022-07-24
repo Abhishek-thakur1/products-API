dd872d246b778100d40eb9d2e98f1ce1511cd4ec
Abhishek-thakur1 <abhishekcool1804@gmail.com>
2022-07-24 18:27:50 +0530

    Refresh Access Token Controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,9 +1,11 @@
 import { CookieOptions, NextFunction, Response, Request } from 'express'
 import {createUserInput, loginUserInput} from '../schemas/user.schema'
 import config = require('config')
-import { createUser, findUserByEmail, signTokens } from 'src/services/user.service'
+import { createUser, findUserByEmail, findUserById, signTokens } from 'src/services/user.service'
 import {User} from '../entities/user.entity'
 import AppError from 'src/utils/appError'
+import { signJwt, verifyJwt } from 'src/utils/jwt'
+import redisClient from 'src/utils/connectRedis'
 
 
 const cookieOptions: CookieOptions = {
@@ -103,3 +105,65 @@ export const loginUserHandler = async (
         next(err);
     }
 }
+
+
+// Refresh Access Token Controller
+export const refreshAccessTokenHandler = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const refresh_token = req.cookies.refresh_token;
+
+        const message = 'Could not refresh access token';
+
+        if (!refresh_token) {
+            return next(new AppError(403, message));
+        }
+
+        // Validate refresh token
+        const decoded = verifyJwt<{ sub: string }>(
+            refresh_token,
+            'refreshTokenPublicKey'
+        );
+
+        if (!decoded) {
+            return next(new AppError(403, message));
+        }
+
+        // Check if user has a valid session
+        const session = await redisClient.get(decoded.sub);
+
+        if (!session) {
+            return next(new AppError(403, message));
+        }
+
+        // Check if user still exist
+        const user = await findUserById(JSON.parse(session).id);
+
+        if (!user) {
+        return next(new AppError(403, message));
+        }
+
+        // Sign new access token
+        const access_token = signJwt({ sub: user.id }, 'accessTokenPrivateKey', {
+            expiresIn: `${config.get<number>('accessTokenExpiresIn')}m`,
+        });
+
+        // Add Cookies
+        res.cookie('access_token', access_token, accessTokenCookieOptions);
+        res.cookie('logged_in', true, {
+            ...accessTokenCookieOptions,
+            httpOnly: false,
+        });
+
+        // Send response
+        res.status(200).json({
+            status: 'success',
+            access_token,
+        });
+    } catch (err: any) {
+        next(err);
+    }
+};
