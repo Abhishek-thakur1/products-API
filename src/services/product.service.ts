37be61e2d3e9bf7a3ee2a1ca83cec053fb485312
Abhishek-thakur1 <abhishekcool1804@gmail.com>
2022-07-27 19:10:35 +0530

    added products services

---
+++ src/services/product.service.ts
@@ -0,0 +1,21 @@
+import {
+    FindOptionsRelations,
+    FindOptionsSelect,
+    FindOptionsWhere,
+    getRepository,
+} from "typeorm";
+import { Product } from '../entities/product.entity'
+import {User} from '../entities/user.entity'
+import {AppDataSource} from '../utils/data-source'
+
+const productRepository = AppDataSource.getRepository(Product)
+
+export const createProduct = async (input: Partial<Product>) => {
+    return await productRepository.save(productRepository.create({...input}))
+}
+
+export const getProduct = async (productId: string) => {
+    return await productRepository.findOneBy({ id: productId})
+}
+
+
